df8e1ca8cfcfdaec70462926b540d89379338b68
satyainv <satyainv459@gmail.com>
2022-07-16 00:35:01 +0200

    committing all files

---
+++ src/main/java/com/satya/sonarqubeproject/SonarqubeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.satya.sonarqubeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satya/sonarqubeproject/SonarqubeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satya.sonarqubeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
